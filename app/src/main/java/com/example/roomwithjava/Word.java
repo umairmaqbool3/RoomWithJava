1fa3adb2a116a82c1f698d494adc9a0878cd42d6
Umair Maqbool <umair.maqbool55@gmail.com>
2023-02-27 22:45:23 +0500

    Upload Files

---
+++ app/src/main/java/com/example/roomwithjava/Word.java
@@ -0,0 +1,26 @@
+package com.example.roomwithjava;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+
+    public void setWord(@NonNull String mWord) {
+        this.mWord = mWord;
+    }
+}
